d11d9c46b20faeadcf805f838f5b8b8cd79ef388
Florian <florian-1992@hotmail.fr>
2021-03-31 00:59:43 +0200

    J'ai juste fait un petit hello world associé au bouton 'help'

--- codes/src/controller/SisSceneController.java
+++ codes/src/controller/SisSceneController.java
@@ -1,4 +1,17 @@
 package controller;
 
-public class SisSceneController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SisSceneController
+{
+    @FXML
+    private TextField dialogTextField;
+
+    @FXML
+    private void printHelp()
+    {
+        dialogTextField.setText("Hello World!");
+    }
 }
