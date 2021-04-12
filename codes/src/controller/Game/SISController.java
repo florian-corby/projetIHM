bc4b6b1e785e5075970e998901a25d96102f7974
Florian <florian-1992@hotmail.fr>
2021-04-12 22:22:34 +0200

    Le bouton help() marche, la fonction de sauvegarde semble marcher aussi. Beaucoup de travail à faire dans le code!

--- codes/src/controller/Game/SISController.java
+++ codes/src/controller/Game/SISController.java
@@ -1,15 +1,8 @@
 package controller.Game;
 
 import controller.Location.ShipController;
-import javafx.scene.Parent;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.Serializable;
-import java.util.Scanner;
 
 public class SISController implements Serializable {
 
@@ -20,8 +13,6 @@ public class SISController implements Serializable {
 	{
 		this.dialogTextField = dialogTextField;
 		this.initGame();
-		//this.play();
-		//this.endGame();
 	}
 
 	public ShipController getShip()
@@ -31,17 +22,11 @@ public class SISController implements Serializable {
 
 	public void initGame()
 	{
-		this.printGameIntro();
-		this.printScenario();
-
-		dialogTextField.setText("\t\t ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n\n");
 		dialogTextField.setText("You wake up feeling dizzy. Something is talking to you. Something not human.\n");
 		ship = new ShipController();
 		ship.getShipModel().getNPC("Kilen").talk();
 		ship.getShipModel().getNPC("Kilen").give("passT", ship.getShipModel().getPlayer());
 		ship.getShipModel().getNPC("Kilen").setSpeech("You should hurry! I've managed to deal with the guards in the lab but it won't be long before they come back!");
-
-		//ship.getShipModel().getPlayer().setSIS(this);
 	}
 
 	public boolean isEndGame()
@@ -55,66 +40,7 @@ public class SISController implements Serializable {
 
 	public void endGame()
 	{
-		dialogTextField.setText("\n\n\t\t ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n");
+		dialogTextField.setText("\n\n\t\t ~~~~~~~~~~~ \n");
 		dialogTextField.setText("Thanks for playing Silent In Space! And special thanks to our beta-tester Ophélie De Sousa Oliveira :) !\n");
-	}
-
-	/*public void load()
-	{
-		try {
-			FileInputStream fileIn = new FileInputStream("saveData.txt");
-			ObjectInputStream ois = new ObjectInputStream(fileIn);
-			ship = new ShipController((Ship) ois.readObject());
-			ois.close();
-			dialogTextField.setText("You successfully loaded the game!\n");
-			dialogTextField.setText("\t\t ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n\n");
-		} catch (IOException | ClassNotFoundException e) {
-			dialogTextField.setText("No save data was found! You need to save at least one time before being able to load a save.");
-			dialogTextField.setText("\t\t ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n\n");
-		}
-	}*/
-
-	/*public void play()
-	{
-		while(!this.isEndGame())
-		{
-			this.playTurn();
-		}
-
-		if(ship.getShipModel().getNPC("Umhon").isDead())
-			dialogTextField.setText("\nYou managed to escape but Umhon couldn't stop her husband and his commander. Thus, the aliens" +
-					" managed to create a virus which decimated all the human population. You were the last survivor and witnessed " +
-					"the fruits of your own actions.");
-
-		else
-			dialogTextField.setText("\nYou managed to escape and Umhon stopped the experiments on the human beings. Instead, the aliens chose a more " +
-					"diplomatic way to settle down on Earth by reaching out to the European Space Agency of Toulouse-Matabiau.");
-	}*/
-
-	/*public void playTurn()
-	{
-		ship.getShipModel().getPlayer().call();
-	}*/
-
-	public void printGameIntro()
-	{
-		dialogTextField.setText("\t\t ========================================= ");
-		dialogTextField.setText("\t\t ============ SILENT IN SPACE ============ ");
-		dialogTextField.setText("\t\t ========================================= ");
-
-		dialogTextField.setText("\nWELCOME to Silent In Space! This game was developed by Florian Legendre, Alexis Louail\n" +
-				"and Vincent Tourenne as a universitary project. This is a demo, hence all the features\nintended to be " +
-				"in the final version aren't there. This game is meant to be played by\ntextual commands. Meaning that " +
-				"you must input valid commands with your keyboard and\nthe game will react accordingly. For a thorough " +
-				"listing of commands, their syntaxes\nand effects, type help! Enjoy!\n");
-	}
-
-	public void printScenario()
-	{
-		dialogTextField.setText("SCENARIO: You wake up in an alien ship. You understand that you've been abducted and\nyou " +
-				"must escape. Yet, you can't use the escape pods of the ship without a code.\nUmhon, an important alien " +
-				"person, can give you this code (OR you can take it from her\nbody) but you have to bring her the proof of " +
-				"the abominable experiments being conducted\non humans. This proof is what will end the abductions and " +
-				"possibly the end of humanity.\nThe escape room is ROOM 13. Good luck human!\n");
 	}
 }
--- codes/src/controller/SisSceneController.java
+++ codes/src/controller/SisSceneController.java
@@ -2,6 +2,7 @@ package controller;
 
 import controller.Game.SISController;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -11,7 +12,11 @@ import java.io.ObjectOutputStream;
 
 public class SisSceneController
 {
-    private SISController gameSISControllerController;
+    private SISController gameSISController;
+    private String previousDialog = "";
+
+    @FXML
+    private Button helpButton;
 
     @FXML
     private TextArea dialogTextField;
@@ -19,7 +24,37 @@ public class SisSceneController
     @FXML
     private void printHelp()
     {
-        dialogTextField.setText("Hello World!");
+        String currentDialog = dialogTextField.getText();
+
+        String helpDialog = """
+                -- User Manual of Silent In Space --\s
+
+                WELCOME to Silent In Space! This game was developed by Florian Legendre, Alexis Louail
+                and Vincent Tourenne as a universitary project. This is a demo, hence all the features
+                intended to be in the final version aren't there. This game is meant to be played by
+                textual commands. Meaning that you must input valid commands with your keyboard and
+                the game will react accordingly. For a thorough listing of commands, their syntaxes
+                and effects, type help! Enjoy!
+
+                SCENARIO: You wake up in an alien ship. You understand that you've been abducted and
+                you must escape. Yet, you can't use the escape pods of the ship without a code.
+                Umhon, an important alien person, can give you this code (OR you can take it from her
+                body) but you have to bring her the proof of the abominable experiments being conducted
+                on humans. This proof is what will end the abductions and possibly the end of humanity.
+                The escape room is ROOM 13. Good luck human!
+
+                """;
+
+        if(!currentDialog.equals(helpDialog)) {
+            previousDialog = currentDialog;
+            dialogTextField.setText(helpDialog);
+            helpButton.setText("Back to the Game");
+        }
+
+        else {
+            helpButton.setText("?");
+            dialogTextField.setText(previousDialog);
+        }
     }
 
     @FXML
@@ -28,9 +63,9 @@ public class SisSceneController
         try {
             FileOutputStream fileOut = new FileOutputStream("saveData.txt");
             ObjectOutputStream oos = new ObjectOutputStream(fileOut);
-            oos.writeObject(gameSISControllerController.getShip());
+            oos.writeObject(gameSISController.getShip());
             oos.close();
-            System.out.println("You successfully saved the game!");
+            dialogTextField.setText(dialogTextField.getText() + "\n\n --- You successfully saved the game! --- ");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -42,6 +77,6 @@ public class SisSceneController
 
     public void setGame(SISController sisController)
     {
-        gameSISControllerController = sisController;
+        gameSISController = sisController;
     }
 }
